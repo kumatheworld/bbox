a5cd1999f8994d9e6ae0c91ed6f28d6fe41068d7
kumatheworld <kumatheworld1105@yahoo.co.jp>
2021-11-15 22:03:44 +0900

    Create bbox.py

---
+++ bbox/bbox.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class BBox:
+    origin = 1
+
+    def __init__(self, arr, mode="xyxy", origin=1, copy=False):
+        """mode = 'xyxy' | 'xywh' | 'ccwh'"""
+
+        if mode not in ("xyxy", "xywh", "ccwh"):
+            raise ValueError("mode must be 'xyxy', 'xywh', or 'ccwh'")
+
+        if isinstance(arr, np.ndarray):
+            if copy:
+                arr = arr.copy()
+        else:
+            arr = np.array(arr)
+
+        if arr.shape[-1] != 4:
+            raise ValueError(
+                "The last dimension of the array must be of size 4, "
+                "but the given array shape is " + str(arr.shape)
+            )
+
+        if mode == "xywh":
+            arr[..., 2:] += arr[..., :2] - 1
+        elif mode == "ccwh":
+            arr[..., :2] -= (arr[..., 2:] - 1) / 2
+            arr[..., 2:] += arr[..., :2] - 1
+
+        self._xyxy = arr
+        if origin != self.origin:
+            self._xyxy += self.origin - origin
