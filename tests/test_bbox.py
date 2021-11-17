2c57de9c8c31e3d592e92bacca6180a449facc4b
kumatheworld <kumatheworld1105@yahoo.co.jp>
2021-11-17 21:37:46 +0900

    Create test_bbox.py

---
+++ tests/test_bbox.py
@@ -0,0 +1,15 @@
+from unittest import TestCase, main
+
+import numpy as np
+from bbox.bbox import BBox
+
+
+class TestBBox(TestCase):
+    def test_init(self):
+        arr = np.random.rand(2, 3, 4)
+        bbox = BBox(arr)
+        self.assertIsInstance(bbox, BBox)
+
+
+if __name__ == "__main__":
+    main()
