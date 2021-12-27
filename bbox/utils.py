acc3b3731491f45aec54a04a794215f3fec0f6d9
kumatheworld <kumatheworld1105@yahoo.co.jp>
2021-12-27 19:32:11 +0900

    Define Pariable and Pair for pair()

--- bbox/utils.py
+++ bbox/utils.py
@@ -1,10 +1,13 @@
-from typing import Any, Sequence, Union
+from typing import Any, Sequence, Union, cast
+
+Pairable = Union[Any, Sequence[Any]]
+Pair = tuple[Any, Any]
 
 
-def pair(x: Union[Any, Sequence[Any]]) -> tuple[Any, ...]:
+def pair(x: Pairable) -> Pair:
     try:
         if len(x) == 2:
-            return tuple(x)
+            return cast(Pair, tuple(x))
         else:
             raise ValueError(f"object length should be 2 but was {len(x)}")
     except TypeError:
