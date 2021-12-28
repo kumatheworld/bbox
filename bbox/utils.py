1560c22b9f7f861e7f2c4e9e3bdbe530f9e884df
kumatheworld <kumatheworld1105@yahoo.co.jp>
2021-12-28 13:10:39 +0900

    Use TypeVar for pair()

--- bbox/utils.py
+++ bbox/utils.py
@@ -1,14 +1,16 @@
-from typing import Any, Sequence, Union, cast
+from collections.abc import Collection
+from typing import TypeVar, Union, cast
 
-Pairable = Union[Any, Sequence[Any]]
-Pair = tuple[Any, Any]
+T = TypeVar("T")
+Pairable = Union[T, Collection[T]]
+Pair = tuple[T, T]
 
 
-def pair(x: Pairable) -> Pair:
+def pair(x: Pairable[T]) -> Pair[T]:
     try:
         if len(x) == 2:
-            return cast(Pair, tuple(x))
+            return cast(Pair[T], tuple(x))
         else:
             raise ValueError(f"object length should be 2 but was {len(x)}")
     except TypeError:
-        return x, x
+        return cast(Pair[T], (x, x))
